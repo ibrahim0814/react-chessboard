38df0624219c8bc3183998b871292a7c799f4f73
ibrahim0814 <ibrahim.a.ali@vanderbilt.edu>
2018-03-31 23:40:33 -0500

    alt img description

--- src/components/square.js
+++ src/components/square.js
@@ -3,7 +3,7 @@ import React from 'react';
 const square = (props) => {
     let link = <div></div>
     if(props.piece !== "none"){
-        link = <img src={require("../assets/pieces/"+props.piece+".png")} alt ="wierd"/>
+        link = <img src={require("../assets/pieces/"+props.piece+".png")} alt ="an amazing chess piece"/>
     }
 
     return(
